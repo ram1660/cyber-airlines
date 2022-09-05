e3e4d5af2a87f77b78cb4a558e42a38d15ad4044
Ram Torgeman <ramtorgeman@gmail.com>
2022-09-06 00:03:26 +0300

    Added server side routes files and JWT package.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,30 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 import { json } from "body-parser";
-
+import * as dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import cors from "cors";
+import { FlightSearchRoutes } from "./routes/flightSearchRoutes";
 
 const app = express();
 app.use(json());
 app.use(cors());
 
-interface User {
-  name: string;
-  about: string;
-  avatar: string;
-  id: string;
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.get('Authorization');
+  const token = authHeader ? authHeader.split(' ')[1];
+  if (token === null) return res.status(401);
+  jwt.
 }
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send(readFileData);
-});
+function generateAccessTocket(user: { username: string, password: string}): void {
+  return jwt.sign(user, process.env.)
+}
+
+// Middleware for authenticating a user
+app.use(authenticateToken);
+
+// Routes
 
 
 app.listen(3000);
