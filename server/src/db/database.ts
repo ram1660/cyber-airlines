80f0400406b30cbe8a3ff47a32e8ee211f30c351
Ram Torgeman <ramtorgeman@gmail.com>
2022-09-06 16:54:39 +0300

    Set up a mongoDB database

---
+++ server/src/db/database.ts
@@ -0,0 +1,61 @@
+import mongoose, { connect, Schema, model, Types, Model } from "mongoose";
+
+
+// The main documents for the database.
+interface IAirline {
+    airlineId: string;
+    airlineName: string;
+    password: string;
+    token: string;
+}
+
+interface ITicket {
+    ticketId: string;
+    airlineOperator: IAirline;
+    quantity: number;
+    fromAirport: string;
+    toAirport: string;
+}
+
+interface ICustomer {
+    username: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    token: string;
+    boughtTickets: Types.DocumentArray<ITicket>;
+}
+
+const ticketsSchema = new Schema<ITicket> ({
+    airlineOperator: { type: String, required: true},
+    fromAirport: { type: String, required: true },
+    toAirport: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    ticketId: { type: String, required: true },
+})
+
+const customerSchema = new Schema<ICustomer>({
+    username: { type: String, required: true },
+    firstName: {type: String, required: true },
+    lastName: {type: String, required: true },
+    password: { type: String, required: true },
+    token: { type: String, required: true},
+    boughtTickets: { type: [ticketsSchema], required: true }
+});
+
+const airlineSchema = new Schema<IAirline>({
+    airlineId: { type: String, required: true },
+    airlineName: { type: String, required: true },
+    password: { type: String, required: true },
+    token: { type: String, required: true },
+});
+
+// Models
+const Customer = model<ICustomer>('Customers', customerSchema);
+const Ticket = model<ITicket>('Tickets', ticketsSchema);
+const Airline = model<IAirline>('Airlines', airlineSchema);
+
+export async function startDB(): Promise<void> {
+    await connect('mongodb://localhost:27017/airlinesDB');
+
+}
