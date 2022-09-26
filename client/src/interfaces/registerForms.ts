da7513fe0fdc6b14a4f7f0525933af98aaaa1d40
Ram Torgeman <ramtorgeman@gmail.com>
2022-09-26 23:48:20 +0300

    Added interfaces to match server side request.

---
+++ client/src/interfaces/registerForms.ts
@@ -0,0 +1,12 @@
+export interface CustomerRegisterForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface AirlineRegisterForm {
+    airlineName: string;
+    email: string;
+    password: string;
+}
