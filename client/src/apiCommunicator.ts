67ca16954245cbe4209d7e4cdad45f9ccedc5eb4
Ram Torgeman <ramtorgeman@gmail.com>
2022-09-26 23:46:29 +0300

    Added functions for client side API calls.

--- client/src/apiCommunicator.ts
+++ client/src/apiCommunicator.ts
@@ -1,1 +1,10 @@
-export {};
+import { AirlineRegisterForm, CustomerRegisterForm } from "./interfaces/registerForms";
+
+export async function registerCustomer(form: CustomerRegisterForm): Promise<void> {
+
+}
+
+export async function registerAirline(form: AirlineRegisterForm): Promise<void> {
+
+    
+}
