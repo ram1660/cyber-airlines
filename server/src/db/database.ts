0411220882cf9dcd5da4406032899e35fa7e1815
Ram Torgeman <ramtorgeman@gmail.com>
2022-09-27 15:49:09 +0300

    Added a refresh token DB. Tweaked DB schemas.

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -3,14 +3,13 @@ import mongoose, { connect, Schema, model, Types, Model } from "mongoose";
 
 // The main documents for the database.
 export interface IAirline {
-    airlineId: string;
+    airlineUsername: string;
     airlineName: string;
     password: string;
     token: string;
 }
 
 export interface ITicket {
-    ticketId: string;
     airlineOperator: IAirline;
     quantity: number;
     fromAirport: string;
@@ -26,36 +25,45 @@ export interface ICustomer {
     boughtTickets: Types.DocumentArray<ITicket>;
 }
 
+// Folds all the refresh tokens of any users.
+export interface IRefreshToken {
+    token: string;
+}
+
 // The schemas
+
+const refreshTokenSchema = new Schema<IRefreshToken>({
+    token: { type: String, required: true, unique: true}
+});
 
 const ticketsSchema = new Schema<ITicket>({
     airlineOperator: { type: String, required: true },
     fromAirport: { type: String, required: true },
     toAirport: { type: String, required: true },
     quantity: { type: Number, required: true },
-    ticketId: { type: String, required: true },
-})
+});
 
 const customerSchema = new Schema<ICustomer>({
     username: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     password: { type: String, required: true },
-    token: { type: String, required: true, unique: true },
+    token: { type: String, required: false, },
     boughtTickets: { type: [ticketsSchema], required: true }
 });
 
 const airlineSchema = new Schema<IAirline>({
-    airlineId: { type: String, required: true, unique: true },
     airlineName: { type: String, required: true, unique: true },
+    airlineUsername: { type: String, required: true, unique: true},
     password: { type: String, required: true },
-    token: { type: String, required: true },
+    token: { type: String, required: false },
 });
 
 // Models
 export const Customer = model<ICustomer>('Customers', customerSchema);
 export const Ticket = model<ITicket>('Tickets', ticketsSchema);
 export const Airline = model<IAirline>('Airlines', airlineSchema);
+export const RefreshToken = model<IRefreshToken>('RefreshTokens', refreshTokenSchema);
 
 export async function startDB(): Promise<void> {
     await connect('mongodb://localhost:27017/airlinesDB');
