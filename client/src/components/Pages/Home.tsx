1460ff3e1d22e80008674e13590db2dc9f5500b6
Ram Torgeman <ramtorgeman@gmail.com>
2022-10-28 15:18:04 +0300

    Added a title and a search bar for the main page.

--- client/src/components/Pages/Home.tsx
+++ client/src/components/Pages/Home.tsx
@@ -1,7 +1,13 @@
+import { CssBaseline, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { WEBSITE_NAME } from '../../globals'
 
 export default function Home() {
   return (
-    <div>This is the home component</div>
+    <>
+      <CssBaseline />
+      <Typography variant='h3' component='h2'>Welcome to {WEBSITE_NAME}! Please search for your airport</Typography>
+      <TextField id='destination-search' label='Destination Search' variant='outlined' />
+    </>
   )
 }
