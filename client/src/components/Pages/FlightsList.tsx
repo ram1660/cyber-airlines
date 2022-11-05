55a2252d8b0f121a94b44f26cb25dd46e89c7f13
Ram Torgeman <ramtorgeman@gmail.com>
2022-11-05 21:37:06 +0200

    Created flights list component for search results.

---
+++ client/src/components/Pages/FlightsList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlightDetailsResponse } from '../../interfaces/AvailableFlights';
+
+
+export default function FlightsList(flights: FlightDetailsResponse) {
+
+    return (
+        <Grid>
+
+        </Grid>
+  )
+}
