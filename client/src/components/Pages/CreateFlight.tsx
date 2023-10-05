f51a373de98be7bf17c52f938fa9ccbebc52ee75
ram-tourgeman <ram.t@spinomenal.com>
2023-10-05 23:29:00 +0300

    Continued working on the create flight page.

--- client/src/components/Pages/CreateFlight.tsx
+++ client/src/components/Pages/CreateFlight.tsx
@@ -1,13 +1,21 @@
 import { Box, Container, CssBaseline, Grid, TextField, ThemeProvider, Typography, createTheme } from '@mui/material'
-import React, { useEffect } from 'react'
+import React, { ChangeEvent, ChangeEventHandler, useEffect, useState } from 'react'
 import AirportSearchBar from '../AirportSearchBar';
+import { DatePicker } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
 const theme = createTheme();
 export default function CreateFlight() {
-
+    const [airportSource, setAirportSource] = useState('');
+    const [airportDestination, setAirportDestination] = useState('');
+    const [amountOfTickets, setAmountOfTickets] = useState(0);
+    const [depatureDate, setDepatureDate] = useState<Dayjs | null>(null);
     useEffect(() => {
 
     }, []);
 
+    const onDateChange = (date: Dayjs | null) => {
+        setDepatureDate(date);
+    }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
@@ -26,7 +34,21 @@ export default function CreateFlight() {
                     <Box component="form" onSubmit={handleSubmit} sx={{mt: 3}}>
                         <Grid container spacing={2}>
                             <Grid item xs={2}>
-                                <AirportSearchBar onSearchInput={}/>
+                               <Typography variant='h3'>From where?</Typography>
+                                <AirportSearchBar searchId={''} searchLabel={''} searchAirportInput={airportSource}/>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography variant='h3'>To where?</Typography>
+                                <AirportSearchBar searchId='destinationAirport' searchLabel='' searchAirportInput={airportDestination}/>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography variant='h3'>How many tickets do you want to sell?</Typography>
+                                <TextField type='number' value={amountOfTickets} onChange={(event) => setAmountOfTickets(parseInt(event.target.value))}/>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography variant='h3'>When will be the flight?</Typography>
+                                <DatePicker minDate={dayjs()}
+                                onChange={}
                             </Grid>
                         </Grid>
                     </Box>
