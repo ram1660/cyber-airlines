e6a1fd652c3ce632852b88c480600b9d28aad0d8
ram-tourgeman <ram.t@spinomenal.com>
2023-10-05 23:30:28 +0300

    Added an informative warning that port is not set up in the .env.

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,15 +32,18 @@ app.listen(process.env.PORT, async () => {
     console.log('Starting server.');
     console.log('Reading dotEnv file...');
     console.log('Grabbing checking tokens existents..');
-    if (process.env['CUSTOMER_TOKEN_SECRET'] === undefined ) {
+    if (process.env['CUSTOMER_TOKEN_SECRET'] === undefined) {
         throw Error('No customer token. Please add the key "CUSTOMER_TOKEN_SECRET" with any value you would like to the .env file.');
     }
     if (process.env['AIRLINE_TOKEN_SECRET'] === undefined) {
         throw Error('No customer token. Please add the key "AIRLINE_TOKEN_SECRET" with any value you would like to the .env file.');
     }
+    if (process.env['PORT'] === undefined) {
+        console.warn('No port number specified! The defualt port the client uses is: 3000. Please enter that port into .env or change it in the the client and .env if you want to use other ports.');
+    }
     console.log('Found secret tokens.');
     await startDB();
     console.log('Database started.');
     console.log('Starting server at http://localhost:' + process.env['PORT'] as string);
-    
+
 });
